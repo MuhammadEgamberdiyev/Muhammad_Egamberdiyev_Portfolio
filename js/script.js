ced7d787408ed153373d97c139435c7510c0ab05
MuhammadEgamberdiyev <egamberdiyevm1141@gmail.com>
2022-11-08 15:57:03 +0500

    demo

--- js/script.js
+++ js/script.js
@@ -139,3 +139,31 @@ $(document).ready(function() {
 
     $('#tag').svg3DTagCloud(settings);
 });
+
+
+document.addEventListener("contextmenu", function (e) {
+    e.preventDefault();
+  });
+  
+  document.onkeydown = function (e) {
+    if (event.keyCode = 123) {
+      return false;
+    }
+  
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "I".charCodeAt(0)) {
+      return false;
+    }
+    
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "C".charCodeAt(0)) {
+      return false;
+    }
+    
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "J".charCodeAt(0)) {
+      return false;
+    }
+    
+    if (e.ctrlKey && e.shiftKey && e.keyCode == "U".charCodeAt(0)) {
+      return false;
+    }
+  
+  }
